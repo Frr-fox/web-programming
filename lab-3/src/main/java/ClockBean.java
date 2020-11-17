f790e0a7952276f6ee98fc0d9ed57c0fc68abe7a
Frr-fox <58375442+Frr-fox@users.noreply.github.com>
2020-11-17 11:49:48 +0300

    Управляемый бин для часов

--- lab-3/src/main/java/ClockBean.java
+++ lab-3/src/main/java/ClockBean.java
@@ -1,22 +1,24 @@
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @ManagedBean(name = "clock")
 @ViewScoped
-public class ClockBean {
-    String time;
-    String day;
+public class ClockBean implements Serializable {
+    private String time;
+    private String day;
 
     public ClockBean() {
         updateTime();
     }
 
-    public void updateTime() {
+    public boolean updateTime() {
         LocalDateTime time = LocalDateTime.now();
         this.time = DateTimeFormatter.ofPattern("HH:mm:ss").format(time);
         this.day = DateTimeFormatter.ofPattern("dd.MM.YY").format(time);
+        return true;
     }
 
     public String getTime() {
